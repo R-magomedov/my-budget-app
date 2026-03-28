d38bb84667609f46fe896240b6221d1f5e037968
R-magomedov <radjab.magomedov1995@gmail.com>
2026-03-29 00:25:19 +0300

    feat: добавил блок баланса с расчетами доходов и расходов

---
+++ src/components/BalanceBlock/BalanceBlock.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react"
+import { TransactionContext } from "../../context/TransactionContext"
+import styles from './BalanceBlock.module.scss'
+
+const BalanceBlock = () => {
+    const { transactions } = useContext(TransactionContext)
+
+    const stats = transactions.reduce((acc, transaction) => {
+        
+        if (transaction.type === 'income') {
+            acc.income += transaction.amount
+            acc.balance += transaction.amount
+        } else {
+            acc.expense += transaction.amount
+            acc.balance -= transaction.amount
+        }
+        
+        return acc
+    }, { income: 0, expense: 0, balance: 0 })
+    const { income, expense, balance } = stats
+    
+  return (
+    <div className={styles.balanceBlock}>
+        <div className={styles.main}>
+            <span className={styles.label}>Баланс</span>
+            <span className={styles.amount}>{balance} ₽</span>
+        </div>
+        <div className={styles.details}>               
+            <div className={styles.income}>
+                <span className={styles.label}>Доход</span>
+                <span className={styles.amount}>+{income} ₽</span>
+            </div>
+            <div className={styles.expense}>
+                <span className={styles.label}>Расход</span>
+                <span className={styles.amount}>-{expense} ₽</span>
+            </div>
+        </div>
+    </div>
+    
+    
+  )
+}
+
+export default BalanceBlock
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import BalanceBlock from '../BalanceBlock/BalanceBlock'
 import styles from './Header.module.scss'
 
 const Header = ({onOpenModal}) => {
   return (
     <div className={styles.header}>
         <h1>💰 Мой бюджет</h1>
+        < BalanceBlock />
         <button onClick={onOpenModal}>Добавить</button>
     </div>
     
